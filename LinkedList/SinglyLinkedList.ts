c9c17e56fc1cd9294bce6d9605f991ff56fe249d
Mohammed-Hamada <mohammed3rbio@gmail.com>
2023-09-17 23:43:39 +0300

    implement some algorithms with linked lists

--- LinkedList/SinglyLinkedList.ts
+++ LinkedList/SinglyLinkedList.ts
@@ -2,6 +2,13 @@ import { SinglyLinkedListNode } from '../Nodes';
 
 export class SinglyLinkedList<T> {
   private _head: SinglyLinkedListNode<T> | null;
+
+  public get head(): SinglyLinkedListNode<T> | null {
+    return this._head;
+  }
+  public set head(value: SinglyLinkedListNode<T> | null) {
+    this._head = value;
+  }
   private _tail: SinglyLinkedListNode<T> | null;
   private _size: number;
 
@@ -129,6 +136,155 @@ export class SinglyLinkedList<T> {
     this._size--;
   }
 
+  nthNodeFromEnd1(n: number): SinglyLinkedListNode<T> | null {
+    let count = 0;
+    let currNode = this._head;
+
+    while (currNode !== null) {
+      currNode = currNode.next;
+      count++;
+    }
+
+    if (n > count) {
+      return null;
+    }
+
+    currNode = this._head;
+    for (let i = 1; i <= count - n; i++) {
+      currNode = currNode?.next!;
+    }
+
+    return currNode;
+  }
+
+  nthNodeFromEnd2(n: number): SinglyLinkedListNode<T> | null {
+    if (this._head === null) {
+      return null;
+    }
+
+    let p = this._head;
+    let f = this._head;
+
+    let count = 1;
+    while (count <= n - 1) {
+      if (f.next === null) {
+        return null;
+      }
+      f = f.next!;
+      count++;
+    }
+
+    while (f.next !== null) {
+      p = p.next!;
+      f = f.next!;
+    }
+
+    return p;
+  }
+
+  isLoopExistsUsingFloyds(): boolean {
+    let slowPtr = this._head;
+    let fastPtr = this._head;
+
+    while (fastPtr !== null && fastPtr.next !== null) {
+      slowPtr = slowPtr?.next!;
+      fastPtr = fastPtr.next.next;
+
+      if (slowPtr === fastPtr) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  findStartOfTheLoop(): SinglyLinkedListNode<T> | null {
+    let fast = this._head;
+    let slow = this._head;
+    let isLoopExists = false;
+
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow?.next!;
+
+      if (fast === slow) {
+        isLoopExists = true;
+        break;
+      }
+    }
+
+    slow = this._head;
+    if (isLoopExists) {
+      while (slow !== fast) {
+        fast = fast?.next!;
+        slow = slow?.next!;
+      }
+      return fast;
+    } else {
+      return null;
+    }
+  }
+
+  findLengthOfTheLoop(): number {
+    let fast = this._head;
+    let slow = this._head;
+    let isLoopExists = false;
+
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow?.next!;
+
+      if (fast === slow) {
+        isLoopExists = true;
+        break;
+      }
+    }
+
+    slow = this._head;
+    if (isLoopExists) {
+      while (slow !== fast) {
+        fast = fast?.next!;
+        slow = slow?.next!;
+      }
+
+      let loopLength = 0;
+      do {
+        loopLength++;
+        fast = fast?.next!;
+      } while (slow !== fast);
+
+      return loopLength;
+    } else return -1;
+  }
+
+  removeNthFromEnd(n: number) {
+    if (n <= 0 || !this._head) {
+      return; // Invalid input or empty list
+    }
+
+    let fast = this._head;
+    let slow = this._head;
+
+    for (let i = 1; i <= n; i++) {
+      if (!fast) {
+        return;
+      }
+      fast = fast?.next!;
+    }
+
+    while (fast?.next) {
+      fast = fast.next;
+      slow = slow?.next!;
+    }
+
+    if (!fast) {
+      this._head = slow?.next!;
+    } else {
+      slow!.next = slow!.next!.next;
+    }
+
+    this._size--;
+  }
+
   indexOf(data: T): number {
     let currNode = this._head;
     let i = 0;
@@ -154,11 +310,11 @@ export class SinglyLinkedList<T> {
 
   toArray(): T[] {
     const result: T[] = [];
-    let current = this._head;
+    let currNode = this._head;
 
-    while (current) {
-      result.push(current.data);
-      current = current.next;
+    while (currNode) {
+      result.push(currNode.data);
+      currNode = currNode.next;
     }
 
     return result;
@@ -188,6 +344,12 @@ list.insert(4, 6); // Insert into index 3
 console.log('\n======== After Insertion At Index 3 =========');
 console.log('To Array:', list.toArray());
 
+console.log('\n==== 3rd Node From End Of List (Brute Force Method) ====');
+console.log(list.nthNodeFromEnd1(3));
+
+console.log('\n==== 3rd Node From End Of List (2 Pointers Method) =====');
+console.log(list.nthNodeFromEnd2(8));
+
 list.deleteFromBeginning();
 
 console.log('\n======== After Delete First Element =========');
@@ -203,10 +365,47 @@ list.delete(3);
 console.log('\n======== After Deletion At Index 3 ==========');
 console.log('To Array:', list.toArray());
 
-console.log('\n============ Index Of Number 7s =============');
+console.log('\n============ Index Of Number 7 ==============');
 console.log(list.indexOf(7));
 
 console.log('\n\n');
 
 console.log('Is Linked List Empty?', list.isEmpty());
 console.log('Size of Linked List:', list.size);
+
+console.log('Is First List Have Loop?', list.isLoopExistsUsingFloyds());
+
+const listWithLoop = new SinglyLinkedList();
+
+const node1 = new SinglyLinkedListNode(1);
+const node2 = new SinglyLinkedListNode(2);
+const node3 = new SinglyLinkedListNode(3);
+const node4 = new SinglyLinkedListNode(4);
+const node5 = new SinglyLinkedListNode(5);
+const node6 = new SinglyLinkedListNode(6);
+const node7 = new SinglyLinkedListNode(7);
+const node8 = new SinglyLinkedListNode(8);
+const node9 = new SinglyLinkedListNode(9);
+const node10 = new SinglyLinkedListNode(10);
+const node11 = new SinglyLinkedListNode(11);
+const node12 = new SinglyLinkedListNode(12);
+
+listWithLoop.head = node1;
+listWithLoop.head.next = node2;
+listWithLoop.head.next.next = node3;
+listWithLoop.head.next.next.next = node4;
+listWithLoop.head.next.next.next.next = node5;
+listWithLoop.head.next.next.next.next.next = node6;
+listWithLoop.head.next.next.next.next.next.next = node7;
+listWithLoop.head.next.next.next.next.next.next.next = node8;
+listWithLoop.head.next.next.next.next.next.next.next.next = node9;
+listWithLoop.head.next.next.next.next.next.next.next.next.next = node10;
+listWithLoop.head.next.next.next.next.next.next.next.next.next.next = node11;
+listWithLoop.head.next.next.next.next.next.next.next.next.next.next.next =
+  node12;
+listWithLoop.head.next.next.next.next.next.next.next.next.next.next.next.next =
+  node7;
+
+console.log('Is First List Have Loop?', listWithLoop.isLoopExistsUsingFloyds());
+console.log('Start Of The Loop:', listWithLoop.findStartOfTheLoop()?.data);
+console.log('Length Of The Loop:', listWithLoop.findLengthOfTheLoop());
